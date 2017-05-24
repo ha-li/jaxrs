eae533cc0fc8de526dd164f4643d3967b828b895
ha-li <ha-li@gmail.com>
2017-05-23 21:24:55 -0700

    example of cookieparam in header to emulate the browser.

--- RESTResources/src/main/java/com/gecko/resources/SubscriptionResource.java
+++ RESTResources/src/main/java/com/gecko/resources/SubscriptionResource.java
@@ -3,6 +3,7 @@ package com.gecko.resources;
 import com.gecko.domain.json.Subscription;
 import com.gecko.rest.headers.GeckoHeaders;
 
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
@@ -20,7 +21,36 @@ public class SubscriptionResource {
 
    @GET
    @Path("{id}")
-   public Response getSubscription (@HeaderParam (GeckoHeaders.CLIENT_ID) String clientId, @PathParam ("id") String userId) {
+   public Response getSubscription (
+           @HeaderParam (GeckoHeaders.CLIENT_ID) String clientId,
+           @PathParam ("id") String userId){
+
+      Subscription subscription = new Subscription ();
+      subscription.setUser(userId);
+      subscription.setId (clientId);
+      return Response.ok().entity(subscription).build();
+   }
+
+
+   /**
+    * Make use of the cookie header. To send a cookie header in postman,
+    * enable the Interceptor feature, then create a header called Cookie:
+    * Cookie: name=value;name2=value2
+    *
+    * based on the cookie named chocolate the header would be
+    * Cookie: sub-id=132846;realm=34223
+    * 
+    * @param clientId
+    * @param userId
+    * @return
+    */
+   @GET
+   @Path("websub/{id}")
+   public Response withCookie (
+           @CookieParam("sub-id") String clientId,
+           @CookieParam("realm") String realmId,
+           @PathParam ("id") String userId) {
+
       Subscription subscription = new Subscription ();
       subscription.setUser(userId);
       subscription.setId (clientId);
