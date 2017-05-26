068b27908ad0d2140422f5332769a713aafffadd
ha-li <ha-li@gmail.com>
2017-05-25 21:49:47 -0700

    exmaple of @Encoded, @Context SecurityContext, and a few other context examples.

--- RESTResources/src/main/java/com/gecko/resources/SubscriptionResource.java
+++ RESTResources/src/main/java/com/gecko/resources/SubscriptionResource.java
@@ -4,16 +4,21 @@ import com.gecko.domain.json.Subscription;
 import com.gecko.rest.headers.GeckoHeaders;
 
 import javax.ws.rs.CookieParam;
+import javax.ws.rs.Encoded;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * Created by hlieu on 05/23/17.
@@ -21,6 +26,25 @@ import javax.ws.rs.core.Response;
 @Path ("subscription")
 @Produces({MediaType.APPLICATION_JSON})
 public class SubscriptionResource {
+
+   @Context
+   private UriInfo uriInfo;
+
+   @Context
+   private Application application;
+
+   @Context
+   private Request request;
+
+   @Context
+   private SecurityContext securityContext;
+
+   // other context you can inject include
+   // Providers (like MessageBodyReader, MessageBodyWriter, ExceptionMapper, ContextResolver
+   // HttpServletRequest
+   // HttpServletResponse
+   // HttpServletContext
+   // ServletConfig
 
    @GET
    @Path("{id}")
@@ -72,6 +96,38 @@ public class SubscriptionResource {
       for(String headerKey : requestHeadermap.keySet ()) {
          System.out.println ("header: " + headerKey + ", value: " + requestHeadermap.get(headerKey));
       }
+
+      // security Context gets injected by the rest engine (Jersey) in this case.
+      // and is a basic interface for a few security related information.
+      System.out.println (securityContext.getUserPrincipal () + " was authenticated by " + securityContext.getAuthenticationScheme () );
+
+      return Response.ok().entity(subscription).build();
+   }
+
+   /*
+    * An example of @Encoded annotation. To see this working:
+    *  1. remove the @Encoded and submit a request to this URI with
+    *      .../encoded/John Blight and see what @param name is initialized as.
+    *  2. repeat but this time add back in @Encoded
+    *
+    *  Now do the same with %20 as white space, eg John%20Blight as the name.
+    *
+    *  @Encoded can be a class level annotation, or a method or a parameter.
+    *  By default JAX-RS decodes all parameters before injecting the values
+    *  into the target variables. With the annotation set, JAX-RS will disable
+    *  the decoding, and inject the encoded value, as demonstrated by the
+    *  example above.
+    *
+    */
+
+   @GET
+   @Path("encoded/{name}")
+   public Response encoded (
+      @Encoded
+      @PathParam("name") String name
+   ) {
+      Subscription subscription = new Subscription ();
+      subscription.setUser(name);
 
       return Response.ok().entity(subscription).build();
    }
