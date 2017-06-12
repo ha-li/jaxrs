46bd0232b1ccc3350ae7389e572a00962b015b1e
ha-li <ha-li@gmail.com>
2017-06-11 20:41:05 -0700

    add configuration resource

--- RESTResources/src/main/java/com/gecko/resources/ConfigurationResource.java
+++ RESTResources/src/main/java/com/gecko/resources/ConfigurationResource.java
@@ -1,7 +1,35 @@
 package com.gecko.resources;
 
+import com.gecko.domain.principal.Principal;
+import com.gecko.schema.subscription.v1.Configuration;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.time.LocalDate;
+
 /**
  * Created by hlieu on 06/11/17.
  */
+@Path ("config")
+@Produces ({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Consumes ({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 public class ConfigurationResource {
+
+   @POST
+   @Path ("add")
+   public Response createConfiguration (Configuration newConf) {
+      Configuration configuration = new Configuration ();
+      configuration.setName ("USE_JMS");
+      configuration.setValue (Boolean.TRUE.toString ());
+      configuration.setCreatedDate (LocalDate.now ());
+      configuration.setCreatedBy (Principal.SYSTEM.name());
+      configuration.setModifiedDate (configuration.getCreatedDate ());
+      configuration.setModifiedBy (configuration.getCreatedBy ());
+
+      return Response.ok().entity(configuration).build();
+   }
 }
--- model/src/main/java/com/gecko/domain/principal/Principal.java
+++ model/src/main/java/com/gecko/domain/principal/Principal.java
@@ -4,4 +4,9 @@ package com.gecko.domain.principal;
  * Created by hlieu on 06/11/17.
  */
 public enum Principal {
+   SYSTEM;
+
+   public String value () {
+      return name();
+   }
 }
