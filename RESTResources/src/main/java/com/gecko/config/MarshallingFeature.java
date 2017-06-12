1d175c48be187b7043a9dfcc0ec7d392f52c7c8a
ha-li <ha-li@gmail.com>
2017-06-11 18:32:18 -0700

    add custome jackson provider with ignore unknown properties

--- RESTResources/src/main/java/com/gecko/config/MarshallingFeature.java
+++ RESTResources/src/main/java/com/gecko/config/MarshallingFeature.java
@@ -1,7 +1,11 @@
-package com.gecko.resources;
+package com.gecko.config;
+
+import com.gecko.provider.CustomJacksonJsonProvider;
 
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
 
 /**
  * Created by hlieu on 05/22/17.
@@ -11,6 +15,7 @@ public class MarshallingFeature implements Feature {
    @Override
    public boolean configure (FeatureContext context) {
       // an empty feature right now
+      context.register (CustomJacksonJsonProvider.class, MessageBodyReader.class, MessageBodyWriter.class);
       return true;
    }
 
