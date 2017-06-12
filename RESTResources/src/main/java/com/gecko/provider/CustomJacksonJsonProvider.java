783f75319e202773306b55c7050d094779ee3ba5
ha-li <ha-li@gmail.com>
2017-06-11 18:30:43 -0700

    add support for unknown properties will not fail

--- RESTResources/src/main/java/com/gecko/provider/CustomJacksonJsonProvider.java
+++ RESTResources/src/main/java/com/gecko/provider/CustomJacksonJsonProvider.java
@@ -1,7 +1,30 @@
 package com.gecko.provider;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
 /**
  * Created by hlieu on 06/11/17.
  */
-public class CustomJacksonJsonProvider {
+@Provider
+@Produces ({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+public class CustomJacksonJsonProvider extends JacksonJaxbJsonProvider {
+   private static ObjectMapper OBJ_MAPPER = new ObjectMapper ();
+
+   static {
+      OBJ_MAPPER.configure (DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+              .enable(SerializationFeature.INDENT_OUTPUT);
+
+   }
+
+   public CustomJacksonJsonProvider () {
+      super();
+      setMapper(OBJ_MAPPER);
+   }
 }
