694e5a7e18ca951c874efb72258f2904fd44253f
ha-li <ha-li@gmail.com>
2017-06-11 18:35:35 -0700

    adapter for xmlgregoriandatetime -> localdate time

--- model/src/main/java/com/gecko/adapter/LocalDateAdapter.java
+++ model/src/main/java/com/gecko/adapter/LocalDateAdapter.java
@@ -1,7 +1,20 @@
 package com.gecko.adapter;
 
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.LocalDate;
+
 /**
  * Created by hlieu on 06/11/17.
  */
-public class LocalDateAdapter {
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+   @Override
+   public LocalDate unmarshal (String input) throws Exception {
+      return LocalDate.parse(input);
+   }
+
+   @Override
+   public String marshal (LocalDate input) throws Exception {
+      return input.toString();
+   }
 }
